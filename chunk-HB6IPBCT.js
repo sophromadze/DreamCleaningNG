d57bb187824d0ae1d6a8235e56360e2b24d20269
sophromadze <sophromadze94@gmail.com>
2025-05-21 17:06:09 +0400

    Auto-generated commit

---
+++ chunk-HB6IPBCT.js
@@ -0,0 +1,1 @@
+import{Ha as o,Va as n,Wa as a,gb as p}from"./chunk-GLNNVTSZ.js";var i=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,m){t&1&&(n(0,"p"),p(1,"about works!"),a())},encapsulation:2})};export{i as AboutComponent};
