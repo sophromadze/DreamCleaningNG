4ca730f3bdbc86579a0362eb81a7682aa2dcb96c
sophromadze <sophromadze94@gmail.com>
2025-07-12 01:08:32 +0400

    some stylings

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  constructor(private router: Router) {}
 
+  navigateToBooking() {
+    this.router.navigate(['/booking']);
+  }
 }
