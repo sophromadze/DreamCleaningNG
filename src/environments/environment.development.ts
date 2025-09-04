287d029fe5b717a1ac795c47a74ff486737f2003
sophromadze <sophromadze94@gmail.com>
2025-09-04 21:04:33 +0400

    google maps api key change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    googleMapsApiKey: 'AIzaSyDwFb_tyXDY8TVH7LJ1NeR72j8b6VWC13k',
+    googleMapsApiKey: 'AIzaSyC8kXlMN-SbzHj616oN5hauYKLPE4mplDg',
     apiUrl: 'http://localhost:5107/api',
     googleClientId: '529008120720-inire1vjeivem8s8830ntrkpbmq1n3fd.apps.googleusercontent.com',
     stripePublishableKey: 'pk_test_51Rj2zA09XH2Z4IpCi9EV0vc5OOx59FLoGW1a9HNy59OrwadL5amuD70JFi6TbH2OwkPSZ27Wvh5DnUJILFHpSmzL00y0ou7fDM',
